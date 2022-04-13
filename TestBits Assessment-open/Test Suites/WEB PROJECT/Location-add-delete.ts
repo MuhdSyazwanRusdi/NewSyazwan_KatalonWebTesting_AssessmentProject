3548fd4358df99ab08246c40abc192c0d0843bcf
MuhdSyazwanRusdi <muhammadsyazwanrusdi@gmail.com>
2022-04-13 16:33:51 +0800

    fsef

---
+++ TestBits Assessment-open/Test Suites/WEB PROJECT/Location-add-delete.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Location-add-delete</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>af267509-5f58-4fd0-b2cc-7cf907ec5b6f</testSuiteGuid>
+   <testCaseLink>
+      <guid>bc77c358-a858-478f-9a85-9cc3f036c489</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PROJECT WEB/000 NORMAL/LOCATION/ADD LOCATION/positive/success add location</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>19f27e54-5f08-41c6-b311-dcf23410aea5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PROJECT WEB/000 NORMAL/LOCATION/DELETE LOCATION/success delete</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
